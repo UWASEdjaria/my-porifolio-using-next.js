9a673488b4feabb8db5e89131116ed15c9365b08
Uwase Djaria <uwasedjaria41@gmail.com>
2025-11-03 21:32:21 +0200

    add styled contact page

---
+++ app/about/page.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import Link from "next/link";
+
+export default function AboutPage() {
+  return (
+    <main className="bg-black font-sans p-5 min-h-screen">
+      {/* Back Home */}
+      <Link
+        href="/"
+        className="text-white border-2 border-purple-800 rounded-lg p-2 hover:border-purple-500 font-bold italic inline-block"
+      >
+        <small>&larr; Back Home</small>
+      </Link>
+
+      {/* About Me Section */}
+      <section className="mt-5 flex flex-col md:flex-row gap-10 max-w-7xl mx-auto">
+        {/* Image on the left */}
+        <div
+          className="flex w-full md:w-1/2 h-64 md:h-96 bg-cover bg-center rounded-lg shadow-lg"
+          style={{ backgroundImage: "url('/background-pic.jpg')" }}
+        ></div>
+
+        {/* Text and Skills on the right */}
+        <div className="flex-1 flex flex-col">
+          <h1 className="text-3xl sm:text-2xl md:text-4xl lg:text-5xl shadow-lg font-bold text-purple-500 text-center mb-5">
+            About Me
+          </h1>
+          <p className="text-white text-lg md:text-xl leading-relaxed">
+            I’m a frontend developer passionate about creating responsive and
+            easy-to-use websites. Skilled in HTML, CSS, and JavaScript, and
+            currently learning UI/UX design to combine clean code with modern
+            design. Always exploring new tools and improving step by step, with
+            the goal of building simple, creative, and meaningful digital
+            experiences.
+          </p>
+
+          {/* Skills BELOW paragraph */}
+          <h2 className="text-white text-3xl font-bold mt-6 mb-4 hover:underline">
+            Skills
+          </h2>
+          <div className="flex flex-wrap gap-6 text-white italic">
+            {["HTML", "CSS", "Tailwind CSS", "JavaScript", "UI/UX Design","React","next"].map(
+              (skill) => (
+                <span
+                  key={skill}
+                  className="bg-purple-600 px-3 py-1 rounded-full text-sm hover:border-purple-500 transition-transform duration-300 hover:scale-105 cursor-pointer"
+                >
+                  {skill}
+                </span>
+              )
+            )}
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
